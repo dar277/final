43c95ca85709f19441b3fc8872384b7a40df4615
vagrant <vagrant@kellogg-dev-box>
2014-06-03 02:28:12 +0000

    First commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,24 @@
 Finalapp::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # Specify a custom home page
+  get "/" => "goals#index"
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  # Resource: Effort
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  # --- Create
+  get "/efforts/new" => 'efforts#new'
+  post "/efforts" => 'efforts#create'
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  # --- Read
+  get "/efforts" => 'efforts#index'
+  get "/efforts/:id" => 'efforts#show'
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  # -- Update
+  get "/efforts/:id/edit" => 'efforts#edit'
+  patch "/efforts/:id" => 'efforts#update'
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+  # --- Delete
+  delete "/efforts/:id" => 'efforts#destroy'
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,23 @@
+Goal.delete_all
+gs = Goal.create("outcome" => "Get in shape")
+mt = Goal.create("outcome" => "Make time for myself")
+
+Strategy.delete_all
+s1 = Strategy.create("description" => "I do cardio 4 times a week", "goal_id" => gs.id)
+s2 = Strategy.create("description" => "I do strength training 3 times a week", "goal_id" => gs.id)
+s3 = Strategy.create("description" => "I sleep 7 hours a night", "goal_id" => mt.id)
+
+Effort.delete_all
+Effort.create("date" => 20140512, "value" => 0, "goal_id" => gs.id, "strategy_id" => s1.id)
+Effort.create("date" => 20140512, "value" => 0, "goal_id" => gs.id, "strategy_id" => s2.id)
+Effort.create("date" => 20140512, "value" => 1, "goal_id" => mt.id, "strategy_id" => s3.id)
+Effort.create("date" => 20140513, "value" => 0, "goal_id" => gs.id, "strategy_id" => s1.id)
+Effort.create("date" => 20140513, "value" => 0, "goal_id" => gs.id, "strategy_id" => s2.id)
+Effort.create("date" => 20140513, "value" => 1, "goal_id" => mt.id, "strategy_id" => s3.id)
+Effort.create("date" => 20140514, "value" => 1, "goal_id" => gs.id, "strategy_id" => s1.id)
+Effort.create("date" => 20140514, "value" => 1, "goal_id" => gs.id, "strategy_id" => s2.id)
+Effort.create("date" => 20140514, "value" => 1, "goal_id" => mt.id, "strategy_id" => s3.id)
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
